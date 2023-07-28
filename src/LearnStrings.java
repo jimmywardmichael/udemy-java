d1d3d32061ceb3ee436646f23ea5e4903be44738
James Ward <jimmywardmichael@gmail.com>
2023-07-28 17:22:34 -0500

    strings to upper and to lower

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -1,11 +1,17 @@
 public class LearnStrings {
     public static void main(String[] args) {
-        String fruit = "apple";
-        String anotherFruit = "apple";
-        String vegetable = new String("broccoli");
-        String anotherVegetable = new String("broccoli");
+//        String fruit = "apple";
+//        String anotherFruit = "apple";
+//        String vegetable = new String("broccoli");
+//        String anotherVegetable = new String("broccoli");
+//
+//        System.out.println(fruit == anotherFruit);//prints true because same part of memory
+//        System.out.println(vegetable == anotherVegetable);//prints false because different part of memory location
 
-        System.out.println(fruit == anotherFruit);
-        System.out.println(vegetable == anotherVegetable);
+        String myText ="abcdefg";
+        System.out.println(myText.toUpperCase());
+
+        String myText2 = "ABCDEFG";
+        System.out.println(myText2.toLowerCase());
     }
 }
