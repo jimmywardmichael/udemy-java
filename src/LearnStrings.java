34b16918b93c4987acb2fa10108803d18a6a771d
James Ward <jimmywardmichael@gmail.com>
2023-07-28 17:17:49 -0500

    strings

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -1,7 +1,11 @@
 public class LearnStrings {
     public static void main(String[] args) {
         String fruit = "apple";
+        String anotherFruit = "apple";
         String vegetable = new String("broccoli");
+        String anotherVegetable = new String("broccoli");
 
+        System.out.println(fruit == anotherFruit);
+        System.out.println(vegetable == anotherVegetable);
     }
 }
