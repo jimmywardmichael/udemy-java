8e23c0e7aaf7d1652172673c8abb12b35c30dc13
James Ward <jimmywardmichael@gmail.com>
2023-07-28 17:55:27 -0500

    replacing words or letters in Strings

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -8,10 +8,25 @@ public class LearnStrings {
 //        System.out.println(fruit == anotherFruit);//prints true because same part of memory
 //        System.out.println(vegetable == anotherVegetable);//prints false because different part of memory location
 
-        String myText ="abcdefg";
-        System.out.println(myText.toUpperCase());
+//        String myText ="abcdefg";
+//        System.out.println(myText.toUpperCase());
+//
+//        String myText2 = "ABCDEFG";
+//        System.out.println(myText2.toLowerCase());
 
-        String myText2 = "ABCDEFG";
-        System.out.println(myText2.toLowerCase());
+//        String myText = "";
+//        System.out.println(myText.isEmpty());//prints true
+//        System.out.println(myText.isBlank());//prints true
+//
+//        String myText2 = "";
+//        System.out.println(myText2.isEmpty());
+//        System.out.println(myText2.isBlank());
+
+//        String myText = "here's my awesome firf text";
+//        System.out.println(myText.replace("firf", "nice"));
+
+
+
+
     }
 }
