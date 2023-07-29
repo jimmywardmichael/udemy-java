344af8de46134b031175adcd34d72e2bceb97f2c
James Ward <jimmywardmichael@gmail.com>
2023-07-29 09:25:54 -0500

    contains method

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -28,9 +28,29 @@ public class LearnStrings {
 //        String firstName = "  Jake  ";
 //        System.out.format("'%s'", firstName.stripIndent());
 
-        String firstName = "       John     ";
-        System.out.format("'%s'", firstName.trim()); //.trim removes whitespace from John leading and trailing
-            //prints out 'John'
+//        String firstName = "       John     ";
+//        System.out.format("'%s'", firstName.trim()); //.trim removes whitespace from John leading and trailing
+        //prints out 'John'
+
+//        String myText = "Apples";
+//        System.out.println(myText.charAt(3));
+
+//        String firstWord = "Dad";
+//        String secondWord = "dad";
+//
+//        System.out.println(compareToIgnoreCase(firstWord, secondWord));
+//
+//    }
+//
+//    public static int compareToIgnoreCase(String text1, String text2) {
+//        String lowerText1 = text1.toLowerCase();
+//        String lowerText2 = text2.toLowerCase();
+//        return lowerText1.compareTo(lowerText2);
+//    }//this prints out 0 when comparing firstWord to secondWord because they are both lowercase and equal.
+
+//        String myText = "Four score and seven years ago";
+//        System.out.println(myText.contains());
+
 
 
     }
