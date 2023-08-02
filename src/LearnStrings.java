5e95e68ade7e795493021c7894fcae36b8c38f34
James Ward <jimmywardmichael@gmail.com>
2023-08-02 18:47:45 -0500

    parsing strings using the split method

--- src/LearnStrings.java
+++ src/LearnStrings.java
@@ -55,33 +55,44 @@ public class LearnStrings {
 //        System.out.println(text1 + text2); //this is my text1 this is my text2
 //        System.out.println(text1.concat(text2)); //this is my text1 this is my text2
 
-        String phoneNumber = "(234) 333-5551";
-        String areaCode = parseAreaCode(phoneNumber);
-        String exchange = parseExchange(phoneNumber);
-        String lineNumber = parseLineNumber(phoneNumber);
+//        String phoneNumber = "(234) 333-5551";
+//        String areaCode = parseAreaCode(phoneNumber);
+//        String exchange = parseExchange(phoneNumber);
+//        String lineNumber = parseLineNumber(phoneNumber);
+//
+//        System.out.println(areaCode);
+//        System.out.println(exchange);
+//        System.out.println(lineNumber);
+//    }
+//
+//    public static String parseAreaCode(String phoneNumber) {
+//        int openParens = phoneNumber.indexOf("(");
+//        int closeParens = phoneNumber.indexOf(")");
+//        String areaCode = phoneNumber.substring(openParens + 1, closeParens);
+//        return areaCode;
+//    }
+//
+//    public static String parseExchange(String phoneNumber) {
+//        int spaceIdx = phoneNumber.indexOf(" ");
+//        int hypenIdx = phoneNumber.indexOf("-");
+//        String exchange = phoneNumber.substring(spaceIdx + 1, hypenIdx);
+//        return exchange;
+//    }
+//
+//    public static String parseLineNumber(String phoneNumber) {
+//        int hyphenIdx = phoneNumber.indexOf("-");
+//        String lineNumber = phoneNumber.substring(hyphenIdx + 1);
+//        return lineNumber;
 
-        System.out.println(areaCode);
-        System.out.println(exchange);
-        System.out.println(lineNumber);
-    }
-
-    public static String parseAreaCode(String phoneNumber) {
-        int openParens = phoneNumber.indexOf("(");
-        int closeParens = phoneNumber.indexOf(")");
-        String areaCode = phoneNumber.substring(openParens + 1, closeParens);
-        return areaCode;
-    }
-
-    public static String parseExchange(String phoneNumber) {
-        int spaceIdx = phoneNumber.indexOf(" ");
-        int hypenIdx = phoneNumber.indexOf("-");
-        String exchange = phoneNumber.substring(spaceIdx + 1, hypenIdx);
-        return exchange;
-    }
-
-    public static String parseLineNumber(String phoneNumber) {
-        int hyphenIdx = phoneNumber.indexOf("-");
-        String lineNumber = phoneNumber.substring(hyphenIdx + 1);
-        return lineNumber;
+        String text = """
+                Smith,Fred,1/1/79,1111 ABC St.,Apple,CA
+                McGuire,Jerry,2/2/80,2222 DEF St.,Orange,NV
+                Flintstone,Fred,3/3/81,3333 GHI St.,Pear,MO
+                Rubble,Barney,4/4/82,4444 JKL St.,Pineapple,IL
+                Jetson,George,5/5/83,5555 MNO St.,Grapefruit,TX
+                """;
+        String[] people = text.split("\n");
+        System.out.println(people.length);
+        //prints out 5 by parsing String into rows
     }
 }
---
+++ src/RegexPractice.java
@@ -0,0 +1,5 @@
+public class RegexPractice {
+    public static void main(String[] args) {
+
+    }
+}
